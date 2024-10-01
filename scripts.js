cf76aaba9a915b606569b4f217d890ca31fb4b9a
NightFalcon8 <8812raghav@gmail.com>
2024-10-01 21:23:23 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// Optional: Smooth scrolling for navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+
